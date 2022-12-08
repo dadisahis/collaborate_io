a638090b731c14c10c95d56aaef131fd479f1061
Adithya Saish <adiwithmj@gmail.com>
2022-12-08 09:17:46 +0530

    created the actiivity component, feed component and more

--- base/models.py
+++ base/models.py
@@ -15,7 +15,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
-    #participants = 
+    participants = models.ManyToManyField(User,related_name='participants',blank=True) 
     updated = models.DateTimeField(auto_now=True) # Difference between autoNow and autoNow_add auto_now saves timestamp each time the object is updated whereas auto_now_add only saves timestamp when an object is created
     created = models.DateTimeField(auto_now_add=True)
     
@@ -32,6 +32,9 @@ class Message(models.Model):
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True) # Difference between autoNow and autoNow_add auto_now saves timestamp each time the object is updated whereas auto_now_add only saves timestamp when an object is created
     created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated','-created']
     
     def __str__(self) -> str:
         return str(self.body[:50])
--- base/views.py
+++ base/views.py
@@ -55,12 +55,14 @@ def home(request):
     rooms = Room.objects.filter(Q(topic__name__icontains=query)| Q(name__icontains=query) | Q(description__icontains=query) | Q(host__username__icontains=query))
     topics = Topic.objects.all()
     rooms_count = rooms.count()
-    context = {'rooms': rooms, 'topics': topics,'room_count': rooms_count}
+    activity_messages = Message.objects.filter(Q(room__topic__name__icontains=query))
+    context = {'rooms': rooms, 'topics': topics,'room_count': rooms_count,'activity_messages': activity_messages}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants= room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
@@ -68,8 +70,10 @@ def room(request,pk):
             room=room,
             body=request.POST.get('body')
         )
+        if request.user not in participants:
+            room.participants.add(request.user)
         return redirect('room',pk=room.id)
-    context = {'room':room , 'room_messages': room_messages }
+    context = {'room':room , 'room_messages': room_messages, 'participants': participants }
     return render(request,'base/room.html',context)
 
 
@@ -110,4 +114,16 @@ def deleteRoom(request,pk):
         return redirect('home')
         
     context = {'obj': room}
+    return render(request,'base/delete.html',context)
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here!')
+    if request.method=='POST':
+        message.delete()
+        return redirect('home')
+        
+    context = {'obj': message}
     return render(request,'base/delete.html',context)
